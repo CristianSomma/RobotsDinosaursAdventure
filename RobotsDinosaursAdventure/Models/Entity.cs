b39a030cb7a14690ad80a534f5a5fd515a17d114
Somma <12999453@itisrossi.vi.it>
2026-02-26 11:36:08 +0100

    entity da ereditare

---
+++ RobotsDinosaursAdventure/Models/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsDinosaursAdventure.Models
+{
+    public abstract class Entity
+    {
+        private readonly int _waitingThreshold;
+        protected static int _entityId = 0;
+
+        protected Entity(uint waitingThreshold = 1000u)
+        {
+            _waitingThreshold = (int)waitingThreshold;
+            _entityId++;
+        }
+
+        protected async Task Wait()
+        {
+            int timeToWait = Random.Shared.Next(0, _waitingThreshold);
+
+            await Task.Delay(timeToWait);
+        }
+    }
+}
