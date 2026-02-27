60e8eb5cefa342377ec5971dd469e703c12e9453
Cristian Somma <12999453@itisrossi.vi.it>
2026-02-27 19:31:47 +0100

    manager terminato

--- RobotsDinosaursAdventure/Managers/MainManager.cs
+++ RobotsDinosaursAdventure/Managers/MainManager.cs
@@ -16,19 +16,38 @@ namespace RobotsDinosaursAdventure.Managers
             _logger = logger;
         }
         
-        public async Task Simulate(uint numberOfRobots, uint numberOfDinosaurs)
+        public async Task Simulate(uint numberOfRobots, uint numberOfDinosaurs, uint portalSize)
         {
             var componentsQueue = new SharedQueue<Component>(_tokenSource.Token);
             var portalStack = new SharedStack<Component>(_tokenSource.Token);
+            List<Task> entitiesActions = new List<Task>();
 
-            _ = Task.Run(async () =>
+            for (int i = 0; i < numberOfRobots; i++)
             {
-                for(int i = 0; i < numberOfRobots; i++)
-                {
-                    Robot newRobot = new Robot(_logger);
-                    newRobot.
-                }
-            }, _tokenSource.Token);
+                Robot newRobot = new Robot(_logger);
+
+                entitiesActions.Add(
+                    newRobot.ProduceComponents(
+                    componentsQueue,
+                    _tokenSource.Token));
+            }
+
+
+            for (int i = 0; i < numberOfDinosaurs; i++)
+            {
+                Dinosaur newDinosaur = new Dinosaur(
+                    portalSize,
+                    _tokenSource,
+                    _tokenSource.Token,
+                    _logger);
+
+                entitiesActions.Add(
+                    newDinosaur.Build(
+                        componentsQueue,
+                        portalStack));
+            }
+
+            await Task.WhenAll(entitiesActions);
         }
     }
 }
