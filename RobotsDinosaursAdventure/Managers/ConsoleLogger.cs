89784a46459f53f3fd798b3afbc0418c237eeea2
12999453 <12999453@SISTEMI23>
2026-02-28 07:52:58 +0100

    organizzazione corretta

--- RobotsDinosaursAdventure/Managers/ConsoleLogger.cs
+++ RobotsDinosaursAdventure/Managers/ConsoleLogger.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace RobotsDinosaursAdventure.Models
+namespace RobotsDinosaursAdventure.Managers
 {
     public class ConsoleLogger : ILogger
     {
